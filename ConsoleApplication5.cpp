b46fd7764c7b5719ad88a3f339ca029b9281af10
Magzim <dotachanger20@gmail.com>
2026-02-09 14:52:57 +0300

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(0, "");
+	
+	unsigned short choice; std::cin >> choice;
+	if (choice == 1) {
+		std::cout << "Сейчас лето, следует одеться по легче.\n";
+	}
+	else if (choice == 2) {
+		std::cout << "Сейчас осень, следует одеться тепло и взять с собой зонт.\n";
+	}
+	else if (choice == 3) {
+		std::cout << "Сейчас зима, следует одеться теплее.\n";
+	}
+	else if (choice == 4) {
+		std::cout << "Сейчас весна, следует одеться по легче и резиновые сапожки.\n";
+	}
+	else {
+		std::cout << "ОШИБКА!!!!!!!!!!.";
+	}
+}
